44e99a55d7433f65c1cb5e6a353e882d4676bed0
Mac <mac@MacBook-Air-Mac.local>
2023-06-14 11:28:45 +0500

    build4

--- build_func04.py
+++ build_func04.py
@@ -1,11 +1,13 @@
 def main(n):
     """A integer type variable 'n' is given. Return the value of the expression in README.md file.
-    https://github.com/codeschool43/Build_in_function_homework#build_func04 
-
+    https://github.com/codeschool43/Build_in_function_homework#build_fun
     Args:
         n (int): integer
         
     Returns:
         float: the value of the expression
     """
-    return
+    value = pow(2+n/3,2)
+    return value
+x=5
+print(main(x))
