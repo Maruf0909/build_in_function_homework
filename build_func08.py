ecd84c1f196972e3a12d5b1b7cdf6ec7d7b41443
Mac <mac@MacBook-Air-Mac.local>
2023-06-14 11:33:21 +0500

    build8

--- build_func08.py
+++ build_func08.py
@@ -9,4 +9,8 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    x=7
+    y=1
+    value = 5*pow(x,2)*pow(y,3)+x*pow(y,2)
+    return value
+print(main(7, 1))
